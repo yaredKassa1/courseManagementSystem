26c20702798ea044182368a0fceb5773e47b315f
yared K <yaredk393@gmail.com>
2025-12-23 10:35:51 +0300

    first commit

---
+++ Backend/src/test/java/com/assignment/courseManagementSystem/CourseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.courseManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
