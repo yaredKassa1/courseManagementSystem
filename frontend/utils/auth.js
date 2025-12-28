208689ec63eb06246fa9c8116890cc9538b5ece4
Mastewal Tilaye <mastewaltilaye12@gmail.com>
2025-12-28 12:26:49 +0300

    utilities

---
+++ frontend/utils/auth.js
@@ -0,0 +1,9 @@
+export const getToken = () => localStorage.getItem("token");
+
+export const setToken = (token) =>
+  localStorage.setItem("token", token);
+
+export const removeToken = () =>
+  localStorage.removeItem("token");
+
+export const isAuthenticated = () => !!getToken();
---
+++ frontend/utils/constants.js
@@ -0,0 +1,6 @@
+export const ROLES = {
+  ADMIN: "ROLE_ADMIN",
+  USER: "ROLE_USER",
+};
+
+export const PAGE_SIZES = [5, 10, 20];
